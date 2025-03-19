5ab6b0fb4ee64a5b6e1cb36ff9c5badfdbc83f95
Ann Mweru <55691219+annmweru@users.noreply.github.com>
2025-03-19 08:13:19 +0300

    deleted the unsed images

--- src/components/ProjectsSection/ProjectsText.jsx
+++ src/components/ProjectsSection/ProjectsText.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 
 
+
 const ProjectsText = () => {
   return (
-    <div className=' flex flex-col items-center mt-[100px]'>
+    <div  id="projects" className=' flex flex-col items-center mt-[100px]'>
         <h2 className='text-6xl text-cyan mb-10'>Projects</h2>
         <p className='text-lg text-center text-white'>
         Here are some of the projects I've worked on, showcasing my skills in front-end development.  
--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+
 const AboutMeText = () => {
+   const scrollToProjects = () => {
+    document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+  }; 
   return (
     <div className=' flex flex-col md:items-start sm:items-center md:text-left'>
         <h2 className='text-6xl text-cyan mb-10' >About Me</h2>
         <p className=' text-white'> I’m a Front-End Developer with 3 years of experience building scalable and engaging web applications. My expertise lies in Angular, JavaScript, and Tailwind CSS, and I’m currently expanding my skills in React to stay ahead in modern front-end development.
 I specialize in creating clean, responsive, and user-friendly interfaces that enhance user experience. My strength is in problem-solving, optimizing performance, and writing maintainable code that scales.
 I’m actively looking for opportunities where I can contribute my skills, collaborate with cross-functional teams, and grow as a developer.</p>
-        <button className=' border border-orange rounded-full py-2 px-4 text-lg -flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan'>My Projects</button>
+        <button    onClick={scrollToProjects}  className=' border border-orange rounded-full py-2 px-4 text-lg -flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan'>My Projects</button>
+        
         </div>
   )
 }
