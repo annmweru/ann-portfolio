44a44e0b20138dbdcdb9bc1f618aeae4c5ff6495
Ann Mweru <55691219+annmweru@users.noreply.github.com>
2025-03-19 13:39:52 +0300

    resume pdf

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 
 function NavbarBtn() {
   return (
-<a href="../../../public/resume.pdf" download="Annah_Mweru_Resume.pdf">     
+<a href="/resume.pdf" download="Annah_Mweru_Resume.pdf">     
  <button className='px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500'>
         Download My Resume      
         <div className='sm:hidden md:block'>       
