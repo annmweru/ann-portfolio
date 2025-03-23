c5419b3e1cea0235cefe83d46efab0a41a7cd5f3
Ann Mweru <55691219+annmweru@users.noreply.github.com>
2025-03-23 11:01:22 +0300

    sm screen responsive

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -5,7 +5,7 @@ function NavbarBtn() {
   return (
 <a href="/resume.pdf" download="Annah_Mweru_Resume.pdf">     
  <button className='px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500'>
-        Download My Resume      
+        My Resume      
         <div className='sm:hidden md:block'>       
           <LuArrowDownRight />
         </div>
--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -12,10 +12,10 @@ const NavbarMain = () => {
   }
   return (
     <nav className='max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2'>
-        <div className='flex justify-between w-full max-w-[1200px] max-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[1.5px] border-orange'> 
+        <div className='flex justify-between w-full max-w-[1200px] max-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange'> 
             <NavbarLogo/>
 
-<div className={`${menuOpen ? "block" : "hidden"} lg:block`}>
+<div className={`${menuOpen ? 'sm:block' : ' sm:hidden'} lg:block`}>
   <NavbarLinks />
 </div>
 
@@ -23,7 +23,7 @@ const NavbarMain = () => {
         <NavbarBtn/>
         </div>
        
-<div className=' flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[1.5px] border-orange'>
+<div className=' flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[0.5px] border-orange'>
   <button className=' text-2xl p-3 border border-orange rounded-full text-white' onClick={toggleMenu}>
     <GiHamburgerMenu />
 
